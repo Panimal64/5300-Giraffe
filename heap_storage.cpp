364f9de865688a2f8642bc1bdca6a4dfbb3f6c10
jaredgmead <jaredgmead>
2018-07-28 12:25:58 -0700

    Added testing to heap_storage.cpp, seg faulting

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -695,65 +695,90 @@ void test_set_row(ValueDict &row, int a, std::string b) {
 	row["b"] = Value(b);
 }
 
+//taken from kLundeen for accurate testing purposes
 bool test_compare(DbRelation &table, Handle handle, int a, std::string b) {
-	ValueDict *result = table.project(handle);
-	Value value = (*result)["a"];
-	if (value.n != a) {
-		delete result;
-		return false;
-	}
-	value = (*result)["b"];
+    ValueDict *result = table.project(handle);
+    Value value = (*result)["a"];
+    if (value.n != a) {
 	delete result;
-	return !(value.s != b);
+	return false;
+    }
+    value = (*result)["b"];
+    delete result;
+    return !(value.s != b);
 }
 
 
 
-/**
 
- * Copy of Kevin Lundeen's test function for heap_storage classes
- * @authore Kevin Lundeen
- * @returns boolean indicating passed tests
- */
-bool test_heap_storage()
-{
-  ColumnNames column_names;
-  column_names.push_back("a");
-  column_names.push_back("b");
-  ColumnAttributes column_attributes;
-  ColumnAttribute ca(ColumnAttribute::INT);
-  column_attributes.push_back(ca);
-  ca.set_data_type(ColumnAttribute::TEXT);
-  column_attributes.push_back(ca);
-  HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
-  table1.create();
-  std::cout << "create ok" << std::endl;
-  table1.drop(); // drop makes the object unusable because of BerkeleyDB restriction -- maybe want to fix this some day
-  std::cout << "drop ok" << std::endl;
+bool test_heap_storage() {
+	ColumnNames column_names;
+	column_names.push_back("a");
+	column_names.push_back("b");
+	ColumnAttributes column_attributes;
+	ColumnAttribute ca(ColumnAttribute::INT);
+	column_attributes.push_back(ca);
+	ca.set_data_type(ColumnAttribute::TEXT);
+	column_attributes.push_back(ca);
+    HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
+    table1.create();
+    std::cout << "create ok" << std::endl;
+    table1.drop();  // drop makes the object unusable because of BerkeleyDB restriction -- maybe want to fix this some day
+    std::cout << "drop ok" << std::endl;
 
-  HeapTable table("_test_data_cpp", column_names, column_attributes);
-  table.create_if_not_exists();
-  std::cout << "create_if_not_exsts ok" << std::endl;
+    HeapTable table("_test_data_cpp", column_names, column_attributes);
+    table.create_if_not_exists();
+    std::cout << "create_if_not_exsts ok" << std::endl;
 
-  ValueDict row;
-  row["a"] = Value(12);
-  row["b"] = Value("Hello!");
-  std::cout << "try insert" << std::endl;
-  table.insert(&row);
-  std::cout << "insert ok" << std::endl;
-  Handles *handles = table.select();
-  std::cout << "select ok " << handles->size() << std::endl;
-  ValueDict *result = table.project((*handles)[0]);
-  std::cout << "project ok" << std::endl;
-  Value value = (*result)["a"];
-  if (value.n != 12)
-    return false;
-  value = (*result)["b"];
-  if (value.s != "Hello!")
-    return false;
-  table.drop();
+    ValueDict row;
+    std::string b = "alkjsl;kj; as;lkj;alskjf;laks df;alsdkjfa;lsdkfj ;alsdfkjads;lfkj a;sldfkj a;sdlfjk a";
+    row["a"] = Value(-1);
+    row["b"] = Value(b);
+    std::cout << "try insert" << std::endl;
+    table.insert(&row);
+    std::cout << "insert ok" << std::endl;
+    Handles* handles = table.select();
+    if (!test_compare(table, (*handles)[0], -1, b)){
+	return false;
+    }
+    std::cout << "select/project ok " << handles->size() << std::endl;
+    delete handles;
 
-  delete handles;
-  delete result;
-  return true;
+    Handle last_handle;
+    int i;
+    for(i = 0; i < 1000; i++){
+	row["a"] = i;
+	row["b"] = b;
+	last_handle = table.insert(&row);
+    }
+    handles = table.select();
+    if (handles->size() != 1001){
+	return false;
+    }
+    for (auto const& handle: *handles){
+	if(!test_compare(table, handle, i++, b)){
+	    return false;
+	}
+    }
+    std::cout << "many inserts/select/projects ok" << std::endl;
+    delete handles;
+
+    table.del(last_handle);
+    handles = table.select();
+    if (handles->size() != 1000){
+	return false;
+    }
+    i = -1;
+    for (auto const& handle: *handles){
+	if(!test_compare(table, handle, i++, b)){
+	    return false;
+	}
+    }
+    std::cout << "del ok" << std::endl;
+
+
+    table.drop();
+	delete handles;
+
+    return true;
 }
